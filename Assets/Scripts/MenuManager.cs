42933654f473e5322cc4971851a35872dfe631f6
Johanna K <93261504+jojofdgshajk@users.noreply.github.com>
2023-12-05 17:21:00 +0100

    Added player and menumanager logic (not working)

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -1,18 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.InputSystem;
 
 public class MenuManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public TMP_Text textToShow1; // Reference to the first TMP_Text component in the Inspector
+    public TMP_Text textToShow2; // Reference to the second TMP_Text component in the Inspector
+
+    public Player player; // Reference to the PlayerTest script
+
+
     void Start()
     {
-        
+
+        textToShow2.gameObject.SetActive(false);
+
+        // Ensure the playerTest variable is assigned in the Inspector or via code
+        if (player == null)
+        {
+            Debug.LogError("PlayerTest reference not set in the Inspector!");
+            return;
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+
+
+        // Check if the player has reached the specific position using a method in the PlayerTest script
+        if (player != null && player.IsAtSpecificPosition())
+        {
+            StartCoroutine(ShowTextTemporarily(textToShow1, 3f, textToShow2));
+            Debug.Log("Start Coroutine.");
+            Debug.Log("Player position: " + player.transform.position);
+        }
+    }
+
+    // Function to show a text without any time constraints
+    void ShowText(TMP_Text textComponent2)
+    {
+        textComponent2.gameObject.SetActive(true);
+    }
+
+    // Function to show a text temporarily for a specified duration
+    IEnumerator ShowTextTemporarily(TMP_Text textComponent, float duration, TMP_Text textComponent2)
+    {
+        yield return new WaitForSeconds(duration);
+        textComponent.gameObject.SetActive(false);
+        ShowText(textComponent2);
     }
 }
